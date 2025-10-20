e2f906a80626d877e2ca272adf739bff496d1b5a
Yucksta <cj.ackee4@gmail.com>
2025-10-20 18:03:24 -0400

    The bug fixed itself?? The project is working well, I think its good. Simplified the code to make it easier to read.

--- python_1/Practice.py
+++ python_1/Practice.py
@@ -1,5 +1,7 @@
 import os
+
 os.system('clear')
+
 Board = [
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
@@ -10,7 +12,25 @@ Board = [
     [0, 0, 0, 0, 0, 0]
 ]
 
-def clear():
+movement = True
+score = [0, 0]
+
+def ACSIIcheck ():
+    def translate(l):
+        if l == 1:
+            return "░░░░░░░░░░░"
+        elif l == 2:
+            return "▓▓▓▓▓▓▓▓▓▓▓"
+        else:
+            return "           "
+
+    for x in range(0, 6):
+        print("/============<>===========<>===========<>===========<>===========<>===========<>===========/|")
+        for y in range(0, 7):
+            print("||" + translate(Board[0][5 - x]) + "||" + translate(Board[1][5 - x]) + "||" + translate(Board[2][5 - x]) + "||" + translate(Board[3][5 - x]) + "||" + translate(Board[4][5 - x]) + "||" + translate(Board[5][5 - x]) + "||" + translate(Board[6][5 - x]) + "||")
+    print("H------1------------2------------3------------4------------5------------6------------7------H")
+
+def wipe():
     for a in range(0, len(Board)):
         for b in range(0, len(Board[a])):
             Board[a][b] = 0
@@ -22,6 +42,17 @@ def verify(col):
             good = True
     return good
 
+def ending(p):
+    score[p - 1] = score[p - 1] + 1
+    ACSIIcheck()
+    print("\n\nPlayer " + str(p) + " victory!   P1: " + str(score[0]) + "  P2: " + str(score[1]))
+    command = str(input())
+    if command == "reset":
+        score[0] = 0
+        score[1] = 0
+    os.system('clear')
+    wipe()
+
 def CHIPplace (col):
     spot = 5
     for i in range(0, 5):
@@ -31,41 +62,21 @@ def CHIPplace (col):
     return spot
 
 def WINcheck (player):
-    os.system('clear')
     for c in range(0, len(Board)):
         for d in range(0, len(Board[c])):
             if Board[c][d] == player:
                 if c < 4:
                     if Board[c + 1][d] == player and Board[c + 2][d] == player and Board[c + 3][d] == player:
-                        print("\n\nPlayer " + str(player) + " victory!----\n\n")
-                        clear()
+                        ending(player)
                 if d > 2:
                     if Board[c][d - 1] == player and Board[c][d - 2] == player and Board[c][d - 3] == player:
-                        print("\n\nPlayer " + str(player) + " victory!vvvv\n\n")
-                        clear()
+                        ending(player)
                 if d > 2 and c > 2:
                     if Board[c - 1][d - 1] == player and Board[c - 1][d - 2] == player and Board[c - 3][d - 3] == player:
-                        print("\n\nPlayer " + str(player) + " victory!////\n\n")
-                        clear()
+                        ending(player)
                 if d > 2 and c < 4:
                     if Board[c + 1][d - 1] == player and Board[c + 2][d - 2] == player and Board[c + 3][d - 3] == player:
-                        print("\n\nPlayer " + str(player) + " victory!-/-/\n\n")
-                        clear()
-
-def ACSIIcheck ():
-    def translate(l):
-        if l == 1:
-            return "░░░░░"
-        elif l == 2:
-            return "▓▓▓▓▓"
-        else:
-            return "     "
-
-    print("---------------------------------------------------")
-    for x in range(0, 6):
-        for y in range(0, 3):
-            print("||" + translate(Board[0][5 - x]) + "||" + translate(Board[1][5 - x]) + "||" + translate(Board[2][5 - x]) + "||" + translate(Board[3][5 - x]) + "||" + translate(Board[4][5 - x]) + "||" + translate(Board[5][5 - x]) + "||" + translate(Board[6][5 - x]) + "||")
-        print("---------------------------------------------------")
+                        ending(player)
 
 def PlayerTurn(p):
     prison = True
@@ -79,18 +90,21 @@ def PlayerTurn(p):
             reward = UFN
     return reward
 
+def cycle(sub):
+    os.system('clear')
+    ACSIIcheck()
+    UFN = PlayerTurn(sub)
+    if UFN == 0:
+        print("\n\n\n\nGame End\n\n\n\n")
+        movement = False
+        return 5
+    Board[UFN - 1][CHIPplace(UFN - 1)] = sub
+    WINcheck(sub)
+
 while True:
-    ACSIIcheck()
-    UFN1 = PlayerTurn(1)
-    if UFN1 == 0:
-        print("\n\n\n\nGame End\n\n\n\n")
+    workspace1 = cycle(1)
+    if workspace1 == 5:
         break
-    Board[UFN1 - 1][CHIPplace(UFN1 - 1)] = 1
-    WINcheck(1)
-    ACSIIcheck()
-    UFN2 = PlayerTurn(2)
-    if UFN2 == 0:
-        print("\n\n\n\nGame End\n\n\n\n")
+    workspace2 = cycle(2)
+    if workspace2 == 5:
         break
-    Board[UFN2 - 1][CHIPplace(UFN2 - 1)] = 2
-    WINcheck(2)
