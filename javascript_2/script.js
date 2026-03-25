7cdac30011024eb00df4d6bcc262d5a69c78be75
Yucksta <cj.ackee4@gmail.com>
2026-03-25 17:16:31 -0400

    I did it (Finished the code)

---
+++ javascript_2/script.js
@@ -0,0 +1,30 @@
+console.log("Code is running. Running where? I don't know.");
+let countvariablethatalsoneedstobethisway = 0;
+let Atleast1iteratedarraywithaccessedandusedelementsinit = [];
+let done206 = [false, "This looks horrible, but this technically still works... right? (please don't grade thos 0/0)"];
+document.getElementById("test").onclick = function() {
+    if (done206[0] == false){
+        done206[0] = true;
+        while (countvariablethatalsoneedstobethisway < 100000) {
+            const documentaryelementalstatus = document.getElementById("test");
+            const temporarylimitvariablethatwasenlargenedtoanunreasonablelengthforschoology = countvariablethatalsoneedstobethisway;
+            function ComputerFrameUpdatingSoftwareDesignedToRepresentTheIllusionOfChioce () {
+                documentaryelementalstatus.style.fontSize =  15 + Math.abs(Math.sin(temporarylimitvariablethatwasenlargenedtoanunreasonablelengthforschoology / 100) * 2) + "px";
+                documentaryelementalstatus.style.color = "rgb(" + Math.abs(Math.sin(temporarylimitvariablethatwasenlargenedtoanunreasonablelengthforschoology / 100)) * 255 + ",0,0)";
+            }
+            if (countvariablethatalsoneedstobethisway % 1300 != 0 && countvariablethatalsoneedstobethisway % 294 != 0){
+                setTimeout(ComputerFrameUpdatingSoftwareDesignedToRepresentTheIllusionOfChioce, countvariablethatalsoneedstobethisway * 2.5);
+            }
+            else if (countvariablethatalsoneedstobethisway == 2009 || countvariablethatalsoneedstobethisway == 17) {
+                //this is not getting ran 01060032 01170058
+                break
+                Atleast1iteratedarraywithaccessedandusedelementsinit[Atleast1iteratedarraywithaccessedandusedelementsinit.length] = countvariablethatalsoneedstobethisway;
+            }
+            else {
+                Atleast1iteratedarraywithaccessedandusedelementsinit[Atleast1iteratedarraywithaccessedandusedelementsinit.length] = countvariablethatalsoneedstobethisway;
+            }
+            countvariablethatalsoneedstobethisway++;
+        }
+        console.error("Data extraction complete; the following state id's have been purged: " + Atleast1iteratedarraywithaccessedandusedelementsinit);
+    }
+ }
