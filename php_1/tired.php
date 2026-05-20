c4a3c51b1490c700c8da47f42f8eab88d4a6ed22
Yucksta <cj.ackee4@gmail.com>
2026-05-20 18:05:49 -0400

    I made a binary calculator in php. The  variable represents the number being converted to binary.

--- php_1/tired.php
+++ php_1/tired.php
@@ -1,5 +1,13 @@
 <?php
-    for ($i = 1; $i <= 30; $i++) {
-        echo($i);
+    $counter = 92;
+    $counter *= 2;
+    $result = [];
+    while ($counter > 0) {
+        $remaining = $counter % 2;
+        $result[sizeof($result)] = $remaining;
+        $counter = floor($counter / 2);
+    };
+    for ($i = sizeof($result) - 1; $i > 0; $i--) {
+        echo $result[$i];
     };
 ?>
